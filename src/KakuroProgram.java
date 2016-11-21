960b2764a35ea638a4b344fb2c93b616937913b4
Leqi Zhao <lzhao@macalester.edu>
2016-11-20 19:49:03 -0600

    Started adding single "dogs" into CheckBoardGraphics

--- src/KakuroProgram.java
+++ src/KakuroProgram.java
@@ -1,16 +1,22 @@
 
 import acm.program.GraphicsProgram;
-import acm.graphics.GLabel;
-import acm.graphics.GObject;
+
+import java.awt.*;
+
 /**
  * Created by Leqi on 11/16/16.
  */
 public class KakuroProgram extends GraphicsProgram {
-    private final int CANVAS_WIDTH = 800;
-    private final int CANVAS_HEIGHT = 800;
+    private final int CANVAS_WIDTH = 500;
+    private final int CANVAS_HEIGHT = 500;
+    private CheckboardGraphics checkBoard;
 
     public void init(){
         setSize(CANVAS_WIDTH,CANVAS_HEIGHT);
+        checkBoard = new CheckboardGraphics(Color.DARK_GRAY);
+
+
+        add(checkBoard, 75, 75);
 
     }
 }
