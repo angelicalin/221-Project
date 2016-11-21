7ae4673dbff9a1b860088f7f5079ec51924d9a55
angelicalin <angelicalin7@gmail.com>
2016-11-20 22:56:17 -0600

    SingleCellConstructer

--- src/SingleCellGraphics.java
+++ src/SingleCellGraphics.java
@@ -1,4 +1,5 @@
 import acm.graphics.GCompound;
+import acm.graphics.GLabel;
 import acm.graphics.GLine;
 import acm.graphics.GRect;
 import com.sun.deploy.util.BlackList;
@@ -12,18 +13,26 @@ public class SingleCellGraphics extends GCompound{
     int valueUp;
     int valueDown;
     boolean fillable;
-    public SingleCellGraphics(int valueUp, int valueDown, boolean fillable){
+    public SingleCellGraphics(Integer valueUp, Integer valueDown, boolean fillable){
         GRect squareCell = new GRect(50,50);
-        GLine seperationLine = new GLine(0,0,50,50);
-        this.add(seperationLine);
+        GLabel labelUp = new GLabel(valueUp.toString());
+        GLabel labelDown = new GLabel(valueDown.toString());
         this.add(squareCell);
-        setCellColor(fillable);
+        this.add(labelDown);
+        this.add(labelUp);
+        this.fillable = fillable;
+        this.valueDown = valueDown;
+        this.valueUp = valueUp;
+        setUpCell();
     }
 
-    private void setCellColor(boolean fillable){
-        if (!fillable){
+    private void setUpCell(){
+        if (!fillable&(valueUp==-1)&(valueDown==-1)){
             this.setColor(Color.BLACK);
-
+        }
+        else if (fillable){
+            GLine seperationLine = new GLine(0,0,50,50);
+            this.add(seperationLine);
         }
     }
 }
