4338afc5baae12f84f3b9431a872e4eb7ccfa97e
angelicalin <angelicalin7@gmail.com>
2016-11-20 22:25:56 -0600

    SingleCellConstructer

--- src/SingleCellGraphics.java
+++ src/SingleCellGraphics.java
@@ -1,3 +1,5 @@
+import acm.graphics.GCompound;
+import acm.graphics.GLine;
 import acm.graphics.GRect;
 import com.sun.deploy.util.BlackList;
 
@@ -6,11 +8,15 @@ import java.awt.*;
 /**
  * Created by Leqi on 11/16/16.
  */
-public class SingleCellGraphics extends GRect{
-
-    public SingleCellGraphics(double xLoc, double yLoc, int value, boolean fillable){
-        super(xLoc,yLoc,50,50);
-        setFilled(true);
+public class SingleCellGraphics extends GCompound{
+    int valueUp;
+    int valueDown;
+    boolean fillable;
+    public SingleCellGraphics(int valueUp, int valueDown, boolean fillable){
+        GRect squareCell = new GRect(50,50);
+        GLine seperationLine = new GLine(0,0,50,50);
+        this.add(seperationLine);
+        this.add(squareCell);
         setCellColor(fillable);
     }
 
