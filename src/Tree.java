f5d59146e91daf47672625534f9453a4975d5b8e
angelicalin <angelicalin7@gmail.com>
2016-11-26 01:01:41 -0600

    add map for nodes

--- src/Tree.java
+++ src/Tree.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -6,12 +7,25 @@ import java.util.List;
  */
 public class Tree {
     private TreeNode root;
+    private HashMap nodesHolder;
 
     public Tree(){
         root = new TreeNode(null, -1);
+        nodesHolder = new HashMap(49);
     }
 
     public TreeNode getRoot (){
         return root;
     }
+
+    public void addChild(TreeNode parentNode, TreeNode childNode, int xLocOfChild, int yLocOfChild){
+        parentNode.addChild(childNode);
+
+        nodesHolder.put()
+    }
+
+    public void removeChildFrom(TreeNode parentNode, TreeNode childNodeToRemove){
+        parentNode.removeChild(childNodeToRemove);
+    }
+
 }
