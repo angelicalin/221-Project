fcb4478501c72e47d0eda387da3dbf309ebdfcba
angelicalin <angelicalin7@gmail.com>
2016-11-25 23:59:44 -0600

    Tree Node Class

---
+++ src/TreeNode.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by angelica on 11/25/16.
+ */
+public class TreeNode {
+    TreeNode parent;
+    int value;
+    List<TreeNode> children = new ArrayList();
+    public TreeNode(TreeNode parent, int value){
+        this.parent = parent;
+        this.value = value;
+    }
+
+    public void addChildren (TreeNode childNode){
+        children.add(childNode);
+    }
+
+    public TreeNode getParent() {
+        return parent;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public List<TreeNode> getChildrenList() {
+        return children;
+    }
+
+    public TreeNode getChildofIndex(int i)
+}
