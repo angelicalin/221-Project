85929af3945bd89bce6e71c748e181659065438b
Leqi Zhao <lzhao@macalester.edu>
2016-11-26 01:30:45 -0600

    Implement GameSolver

--- src/GameSolver.java
+++ src/GameSolver.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Created by angelica on 11/20/16.
  */
@@ -5,6 +7,7 @@ public class GameSolver {
 
     int [][] cell_values;
     Tree solver_tree;
+    ArrayList<Integer> currentNodeValues = new ArrayList<Integer>();
 
 
     public GameSolver(int[][] cell_values){
@@ -17,12 +20,43 @@ public class GameSolver {
             for (int j = 0; j<7; j++) {
                 if (cell_values[i][j] == 0){
                     if(i%2 == 1){
+                        currentNodeValues = findAdjancentRowSum(i,j);
 
+                    } else if (i%2==0) {
 
                     }
                 }
             }
         }
+    }
+
+    public ArrayList<Integer> findAdjancentRowSum(int i, int j) {
+        int numberToAdd = 0;
+        int forbiddenNumber = 0;
+        ArrayList<Integer> possibleValues = new ArrayList<Integer>();
+
+        while(cell_values[i][j] == 0){
+            numberToAdd ++;
+            i--;
+        }
+
+        int rowSum = cell_values[i][j];
+        if(rowSum % numberToAdd == 0){
+            forbiddenNumber = rowSum/numberToAdd;
+        }
+
+
+        for (int k = 1; k< rowSum; k++){
+            possibleValues.add(k);
+            }
+
+        possibleValues.remove(forbiddenNumber);
+
+        return possibleValues;
+
+        }
+
+
     }
 
 
