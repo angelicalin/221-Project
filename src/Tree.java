56db844303f347350769eed45054400e2b9066b5
angelicalin <angelicalin7@gmail.com>
2016-11-26 00:25:55 -0600

    Tree Class

--- src/Tree.java
+++ src/Tree.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by angelica on 11/26/16.
  */
 public class Tree {
     private TreeNode root;
 
-    public Tree(){
+    public Tree(ArrayList FirstChildrenList){
         root = new TreeNode(null, -1);
+
     }
 }
