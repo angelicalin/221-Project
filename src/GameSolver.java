78b53b9ccbea9c20f96c6a013b6d2af839bc54de
Leqi Zhao <lzhao@macalester.edu>
2016-11-29 22:28:27 -0600

    finish .....columnsum in GameSolver

--- src/GameSolver.java
+++ src/GameSolver.java
@@ -9,54 +9,73 @@ public class GameSolver {
     Tree solver_tree;
     ArrayList<Integer> currentNodeValues = new ArrayList<Integer>();
 
+
     public GameSolver(int[][] cell_values){
+
         this.cell_values = cell_values;
-        solver_tree = new Tree();
     }
 
-    public void gameSolve(){
-        for (int i = 0; i <14; i ++){
+    public void GameSolver(){
+        for (int i = 0; i <14; i +=2){
             for (int j = 0; j<7; j++) {
                 if (cell_values[i][j] == 0){
-                    if(i%2 == 1){
-                        findPossibleValuesforRowSum(i,j);
-                    } else if (i % 2 == 0) {
-                        findPossibleValuesforColumnSum(i,j);
+                    findPossibleValuesforRowSum(i,j);
+                    findPossibleValuesforColumnSum(i,j);
                     }
                 }
             }
         }
     }
 
-    private ArrayList<Integer> findPossibleValuesforRowSum(int i, int j) {
+    public ArrayList<Integer> findPossibleValuesforRowSum(int i, int j) {
         int numberToAdd = 0;
+        int forbiddenNumber = 0;
+        ArrayList<Integer> possibleValues = new ArrayList<Integer>();
+
         while(cell_values[i][j] == 0){
             numberToAdd ++;
             j--;
         }
-        ArrayList<Integer> possibleValues=findComposition(cell_values[i][j], numberToAdd);
+
+        int rowSum = cell_values[i][j];
+
+        for (int k = 1; k< rowSum; k++){
+            possibleValues.add(k);
+        }
+
+        possibleValues.remove(forbiddenNumber);
+
         return possibleValues;
-    }
 
-
+        }
 
     private ArrayList<Integer> findPossibleValuesforColumnSum(int i, int j){
         int numberToAdd = 0;
+        int forbiddenNumber = 0;
+        ArrayList<Integer> possibleValues = new ArrayList<Integer>();
+
         while(cell_values[i][j] == 0){
             numberToAdd ++;
             i--;
         }
-        ArrayList<Integer> possibleValues=findComposition(cell_values[i][j], numberToAdd);
+
+        int columnSum = cell_values[i][j];
+
+
+        for (int k = 1; k< columnSum; k++){
+            possibleValues.add(k);
+        }
+
+        possibleValues.remove(forbiddenNumber);
+
         return possibleValues;
+
     }
 
-    private ArrayList<Integer> findComposition(int sum, int num){
-        ArrayList<Integer> result = new ArrayList<>(num);
-        Integer i = sum/num+1;
-        while (i<9 & (sum-i)>0 & i<sum){
-            result.add(i);
-            i++;
-        }
-        return result;
-    }
 }
+
+    }
+
+
+
+
